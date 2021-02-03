0406b29c41e156010ede2218214dae2b7214e347
Бобёр Попёр <Konstantinbobrovski@gmail.com>
2021-02-03 22:38:01 +0200

    Added Boostrap,navbar, not working searchbar.

--- public/FrontEndJS/CreateMap.js
+++ public/FrontEndJS/CreateMap.js
@@ -1,5 +1,6 @@
+
+
 var map = L.map("map").setView([42.6237, 25.3961], 13);
-var fd;
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -9,13 +10,10 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1Ijoia29rb2RxbmtvdiIsImEiOiJja2tvZTB0ZGUybDBwMnZxdGJoN3ZremlrIn0.uwZ7_9LoBPCmeakn1JxkSA'
 }).addTo(map);
 
-
-jQuery.get(window.location.hostname+'/public/BGR.geo.json', function (data) {
-    fd = data;
-    var a = $.parseJSON(data);
-    document.body.innerText = data;
-    L.geoJSON(a).addTo(map);
-});
+//jQuery.get('/BGR.geo.json', function (data) {
+ 
+//    L.geoJSON(data).addTo(map);
+//});
 
 
 
