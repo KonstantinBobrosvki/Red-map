24302ca1ab457e3f2c274f7dc83c039657ff615e
Бобёр Попёр <Konstantinbobrovski@gmail.com>
2021-02-11 22:54:59 +0200

    Removing to hbs rendering

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var http = require('http');
 const express = require('express');
+const hbs = require("hbs");
+
 const app = express();
 const port = process.env.PORT || 3030;
+
+
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-    res.sendfile('public/index.html')
+
+app.set("view engine", "hbs");
+app.set("views", __dirname+"/views");
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.use("/", function (request, response) {   
     
+    response.render("index.hbs");
 });
-app.listen(port, () =>
-    console.log('Express web app available at localhost: ${port}')
-);
+
+
+app.listen(port);
+
