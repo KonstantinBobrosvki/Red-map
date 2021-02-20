bcd854023f4d7557205988be1fa957cbb1822c21
Бобёр Попёр <Konstantinbobrovski@gmail.com>
2021-02-20 13:25:52 +0200

    Created API fro getting location and Implemented

--- API/APIRouter.js
+++ API/APIRouter.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const getCordinatesbyID = require(__dirname + '/getCordinatesbyID.js');
 
 const APIRouter = express.Router();
 
-
+APIRouter.use("/getCordinatesbyID", function (request, response) {
+    getCordinatesbyID.getCordinatesbyID(request, response);
+});
 
 
 
--- public/FrontEndJS/OptionsMenu.js
+++ public/FrontEndJS/OptionsMenu.js
@@ -4,21 +4,39 @@ function Select_bar_change() {
      
         const url = $(this).val();       
         const img_path = "./img/" + url.replace(".html#map", ".jpg");
-              
+        const id = $(this).attr("plant_id");
+        const option_select = $(this);
+        $.ajax({
+            url: "/api/getCordinatesbyID?id=" + id,
+            contentType: "application/json",
+            dataType: 'json',
+            success: function (result) {
+                const cordinates = result;
 
-                var greenIcon = L.icon({
-                    iconUrl: img_path,
+                cordinates.forEach(cordinate => {
+                    var greenIcon = L.icon({
+                        iconUrl: img_path,
 
 
-                    iconSize: [38, 95], // size of the icon
-                    shadowSize: [50, 64], // size of the shadow
-                    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+                        iconSize: [38, 95], // size of the icon
+                        shadowSize: [50, 64], // size of the shadow
+                        iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
 
-                    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+                        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+                    });
+
+                    
+                    L.marker([cordinate.latitude, cordinate.longitude], { icon: greenIcon }).addTo(map).bindPopup("<b> " + option_select.text() + " </b> <br> " + $("#sone").text() + " .").openPopup();
                 });
+            }
+        })
+
+     
+           
+        
 
 
-                L.marker([42.6237 + Math.random(), 25.3961 + Math.random()], { icon: greenIcon }).addTo(map).bindPopup("<b> " + $(this).text() + " </b> <br> " + $("#sone").text() + " .").openPopup();
+               
   
     });
 }
