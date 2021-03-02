5db841c5d860c895f9a8b53b99d0bea04130d1bf
Бобёр Попёр <Konstantinbobrovski@gmail.com>
2021-03-02 18:53:04 +0200

    Design changes in guestbook and validation data for it

--- public/FrontEndJS/guestbookFrontEnd.js
+++ public/FrontEndJS/guestbookFrontEnd.js
@@ -1,13 +1,14 @@
 ﻿
-console.log("loadded");
-
 document.forms["letterForm"].addEventListener("submit", function (e) {
     e.preventDefault();
     
     const form = document.forms["letterForm"];
     const userName = form.elements["userName"].value;
     const letter = form.elements["letter"].value;
-    console.log("OKKK")
+    if (userName.length >= 30 || letter.length >= 200) {
+        alert("Името не трябва да надвишава 30 символа и отзива 200")
+        return;
+    }
     AddLetter(userName, letter);
 
 });
@@ -36,9 +37,10 @@ function reset() {
 }
 
 function Add(letter) {
-
-    var template = "".concat('<p>', letter.username,' : ', letter.letter,'</p>');
-
+    var template = "".concat('<blockquote class="blockquote col-sm-6">',
+        '<p class= "mb-0" >', letter.letter ,'</p >',
+        '<footer class="blockquote-footer">', letter.username,'</footer>',
+       ' </blockquote >')
     $("#messages").append(template);
  
 }
